2a9ed684149d82fd5e6e517c0fdcedf0670b5f31
Ellie <elliemdaw@gmail.com>
2014-09-17 12:38:04 -0600

    Added another line

---
+++ lab4proj/src/hello2.java
@@ -0,0 +1,8 @@
+
+public class hello2 {
+	public static void main(String args[]){
+		System.out.println("Hello world!/n");
+		System.out.println("Learning git...");
+		System.out.println("Again...");
+	}
+}
